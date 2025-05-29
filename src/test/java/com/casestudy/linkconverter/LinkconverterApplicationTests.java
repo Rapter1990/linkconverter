6e9f1bb3fa75be9f3c5c9c93d0a8dd1476d9f710
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-05-29 10:35:36 +0300

    Task 1 : Initial commit

---
+++ src/test/java/com/casestudy/linkconverter/LinkconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casestudy.linkconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
